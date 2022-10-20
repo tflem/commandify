2bd9449ba26a1c67898db1093c70b66d971e04ce
Tim Fleming <tfleming26@hotmail.com>
2022-10-20 16:51:21 -0600

    Update testing configuration

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get welcome" do
-    get root_path
-    assert_response :success
-  end
+   # test "the truth" do
+  #   assert true
+  # end
 end
