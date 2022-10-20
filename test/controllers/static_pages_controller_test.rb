31d9a7f361ff666cdb5a0d54489a4025c3c0e417
Tim Fleming <tfleming26@hotmail.com>
2022-10-20 16:23:18 -0600

    Update controller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get welcome" do
-    get "/welcome"
+    get static_pages_welcome_url
     assert_response :success
   end
 end
